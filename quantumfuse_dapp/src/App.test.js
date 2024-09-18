151964b35dabb12b2763f290ea0a4a4ccfe494a7
Jacque Antoine DeGraff <jacquedegraff@creodamo.com>
2024-09-18 18:49:22 -0400

    Update App.test.js

--- quantumfuse_dapp/src/App.test.js
+++ quantumfuse_dapp/src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Key Features section', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const sectionTitle = screen.getByText(/Key Features/i); // Adjust this to match the actual content
+  expect(sectionTitle).toBeInTheDocument();
 });
